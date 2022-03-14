cba01197e8c9641d04ba89217c7e8378bab1fc3a
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-03-15 00:14:08 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="flex-1">
+        <div className="inside-1">
+          <div className="icon-1">
+            <p>Caner Demir</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-2">
+        <div className="inside-2">
+          <div className="icon-2">
+            PROGRAMING LANGUANGE
+            <p>Javascript</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-2">
+        <div className="inside-2">
+          <div className="icon-2">
+            JAVASCRIPT LIBRARY
+            <p>Reactjs</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-2">
+        <div className="inside-2">
+          <div className="icon-2">
+            STYLE MARKUP LANGUANGE
+            <p>CSS</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-2">
+        <div className="inside-2">
+          <div className="icon-2">
+            TEXT MARKUP LANGUANGE
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          <div className="icon-3">
+            VERSION CONTROL SYSTEM
+            <p>Basic usega of Git</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          REPO HOSTıNG SERVICE
+          <div className="icon-3">
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          PACKAGE MANAGERS
+          <div className="icon-3">
+            <p>npm</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          <div className="icon-3">
+            LINTERS and FORMATTERS
+            <p>Prettier, ESLint</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          PREVENT for PROP DRILLING
+          <div className="icon-3">
+            <p>Familiar with redux and </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-3">
+        <div className="inside-3">
+          CSS FRAMEWORKS
+          <div className="icon-3">
+            <p>Familiar with Reactsrap, Material UI and Bootstrap</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1">
+        <div className="inside-1">
+          <div className="icon-1">
+            <p>My Projects</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
