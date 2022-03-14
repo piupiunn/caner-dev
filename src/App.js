168be8126790f4325511a26e2aaee2f2bfb54712
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-03-15 00:47:48 +0300

    css

--- src/App.js
+++ src/App.js
@@ -5,47 +5,54 @@ function App() {
     <div className="app">
       <div className="flex-1">
         <div className="inside-1">
+          BEN
           <div className="icon-1">
+            <img src="*" alt="*" />
             <p>Caner Demir</p>
           </div>
         </div>
       </div>
       <div className="flex-2">
         <div className="inside-2">
+          PROGRAMING LANGUANGE
           <div className="icon-2">
-            PROGRAMING LANGUANGE
+            <img src="*" alt="*" />
             <p>Javascript</p>
           </div>
         </div>
       </div>
       <div className="flex-2">
         <div className="inside-2">
+          JAVASCRIPT LIBRARY
           <div className="icon-2">
-            JAVASCRIPT LIBRARY
+            <img src="*" alt="*" />
             <p>Reactjs</p>
           </div>
         </div>
       </div>
       <div className="flex-2">
         <div className="inside-2">
+          STYLE MARKUP LANGUANGE
           <div className="icon-2">
-            STYLE MARKUP LANGUANGE
+            <img src="*" alt="*" />
             <p>CSS</p>
           </div>
         </div>
       </div>
       <div className="flex-2">
         <div className="inside-2">
+          TEXT MARKUP LANGUANGE
           <div className="icon-2">
-            TEXT MARKUP LANGUANGE
+            <img src="*" alt="*" />
             <p>HTML</p>
           </div>
         </div>
       </div>
       <div className="flex-3">
         <div className="inside-3">
+          VERSION CONTROL SYSTEM
           <div className="icon-3">
-            VERSION CONTROL SYSTEM
+            <img src="*" alt="*" />
             <p>Basic usega of Git</p>
           </div>
         </div>
@@ -54,7 +61,8 @@ function App() {
         <div className="inside-3">
           REPO HOSTıNG SERVICE
           <div className="icon-3">
-            <p></p>
+            <img src="*" alt="*" />
+            <p>Github</p>
           </div>
         </div>
       </div>
@@ -62,14 +70,16 @@ function App() {
         <div className="inside-3">
           PACKAGE MANAGERS
           <div className="icon-3">
+            <img src="*" alt="*" />
             <p>npm</p>
           </div>
         </div>
       </div>
       <div className="flex-3">
         <div className="inside-3">
+          LINTERS and FORMATTERS
           <div className="icon-3">
-            LINTERS and FORMATTERS
+            <img src="*" alt="*" />
             <p>Prettier, ESLint</p>
           </div>
         </div>
@@ -78,6 +88,7 @@ function App() {
         <div className="inside-3">
           PREVENT for PROP DRILLING
           <div className="icon-3">
+            <img src="*" alt="*" />
             <p>Familiar with redux and </p>
           </div>
         </div>
@@ -86,14 +97,17 @@ function App() {
         <div className="inside-3">
           CSS FRAMEWORKS
           <div className="icon-3">
+            <img src="*" alt="*" />
             <p>Familiar with Reactsrap, Material UI and Bootstrap</p>
           </div>
         </div>
       </div>
       <div className="flex-1">
         <div className="inside-1">
+          My Projects
           <div className="icon-1">
-            <p>My Projects</p>
+            <img src="*" alt="*" />
+            <p>Yes</p>
           </div>
         </div>
       </div>
