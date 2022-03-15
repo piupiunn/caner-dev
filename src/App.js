94a17faf93c8bb0591c4d4f837a45fdc450353a7
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-03-15 17:35:49 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,47 +3,59 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <div className="flex-1">
-        <div className="inside-1">
+      <div className="flex-1-me">
+        <div className="inside-1-me">
           BEN
-          <div className="icon-1">
+          <div className="icon-1-me">
             <img src="*" alt="*" />
             <p>Caner Demir</p>
           </div>
         </div>
       </div>
-      <div className="flex-2">
-        <div className="inside-2">
+      <div className="flex-2-javascript">
+        <div className="inside-2-javascript">
           PROGRAMING LANGUANGE
-          <div className="icon-2">
-            <img src="*" alt="*" />
+          <div className="icon-2-javascript">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/640px-Javascript-shield.svg.png"
+              alt="*"
+            />
             <p>Javascript</p>
           </div>
         </div>
       </div>
-      <div className="flex-2">
-        <div className="inside-2">
+      <div className="flex-2-react">
+        <div className="inside-2-react">
           JAVASCRIPT LIBRARY
-          <div className="icon-2">
-            <img src="*" alt="*" />
+          <div className="icon-2-react">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
+              alt="*"
+            />
             <p>Reactjs</p>
           </div>
         </div>
       </div>
-      <div className="flex-2">
-        <div className="inside-2">
+      <div className="flex-2-css">
+        <div className="inside-2-css">
           STYLE MARKUP LANGUANGE
-          <div className="icon-2">
-            <img src="*" alt="*" />
+          <div className="icon-2-css">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png"
+              alt="*"
+            />
             <p>CSS</p>
           </div>
         </div>
       </div>
-      <div className="flex-2">
-        <div className="inside-2">
+      <div className="flex-2-html">
+        <div className="inside-2-html">
           TEXT MARKUP LANGUANGE
-          <div className="icon-2">
-            <img src="*" alt="*" />
+          <div className="icon-2-html">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
+              alt="*"
+            />
             <p>HTML</p>
           </div>
         </div>
@@ -102,10 +114,10 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="flex-1">
-        <div className="inside-1">
+      <div className="flex-1-works">
+        <div className="inside-1-works">
           My Projects
-          <div className="icon-1">
+          <div className="icon-1-works">
             <img src="*" alt="*" />
             <p>Yes</p>
           </div>
