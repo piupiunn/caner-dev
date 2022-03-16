4f50d0ec22661c7c83b9c47e7ada3df10b6f4cd2
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-03-16 03:09:50 +0300

    css

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       </div>
       <div className="flex-2-javascript">
         <div className="inside-2-javascript">
-          PROGRAMING LANGUANGE
+          <h4>Programing languange</h4>
           <div className="icon-2-javascript">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/640px-Javascript-shield.svg.png"
@@ -26,7 +26,7 @@ function App() {
       </div>
       <div className="flex-2-react">
         <div className="inside-2-react">
-          JAVASCRIPT LIBRARY
+          <h4>Javascript library</h4>
           <div className="icon-2-react">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
@@ -38,7 +38,7 @@ function App() {
       </div>
       <div className="flex-2-css">
         <div className="inside-2-css">
-          STYLE MARKUP LANGUANGE
+          <h4>Style markup languange</h4>
           <div className="icon-2-css">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png"
@@ -50,7 +50,7 @@ function App() {
       </div>
       <div className="flex-2-html">
         <div className="inside-2-html">
-          TEXT MARKUP LANGUANGE
+          <h4> Text markup languange</h4>
           <div className="icon-2-html">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
@@ -60,64 +60,89 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          VERSION CONTROL SYSTEM
-          <div className="icon-3">
-            <img src="*" alt="*" />
+      <div className="flex-3-git">
+        <div className="inside-3-git">
+          <h4>Version control system</h4>
+          <div className="icon-3-git">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png"
+              alt="*"
+            />
             <p>Basic usega of Git</p>
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          REPO HOSTıNG SERVICE
-          <div className="icon-3">
-            <img src="*" alt="*" />
+      <div className="flex-3-github">
+        <div className="inside-3-github">
+          <h4>Repo hosting service</h4>
+          <div className="icon-3-github">
+            <img
+              src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Emblem.png"
+              alt="*"
+            />
             <p>Github</p>
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          PACKAGE MANAGERS
-          <div className="icon-3">
-            <img src="*" alt="*" />
+      <div className="flex-3-npm">
+        <div className="inside-3-npm">
+          <h4>Package manager</h4>
+          <div className="icon-3-npm">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1280px-Npm-logo.svg.png"
+              alt="*"
+            />
             <p>npm</p>
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          LINTERS and FORMATTERS
-          <div className="icon-3">
-            <img src="*" alt="*" />
-            <p>Prettier, ESLint</p>
+      <div className="flex-3-formatters">
+        <div className="inside-3-formatters">
+          <h4>Linters and Formatters</h4>
+          <div className="icon-3-formatters">
+            <img
+              className="img-1"
+              src="https://seeklogo.com/images/P/prettier-logo-D5C5197E37-seeklogo.com.png"
+              alt="*"
+            />
+            <p>Prettier</p>
+            <img
+              className="img-2"
+              src="https://dbaeumer.gallerycdn.vsassets.io/extensions/dbaeumer/vscode-eslint/2.2.3/1642067257652/Microsoft.VisualStudio.Services.Icons.Default"
+              alt=""
+            />
+            <p className="eslint">ESLint</p>
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          PREVENT for PROP DRILLING
-          <div className="icon-3">
-            <img src="*" alt="*" />
-            <p>Familiar with redux and </p>
+      <div className="flex-3-redux">
+        <div className="inside-3-redux">
+          <h4>Alternate of Context</h4>
+          <div className="icon-3-redux">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png"
+              alt="*"
+            />
+            <p> Redux </p>
           </div>
         </div>
       </div>
-      <div className="flex-3">
-        <div className="inside-3">
-          CSS FRAMEWORKS
-          <div className="icon-3">
+      <div className="flex-3-frameworks">
+        <div className="inside-3-frameworks">
+          <h4>Css frameworks</h4>
+          <div className="icon-3-frameworks">
             <img src="*" alt="*" />
-            <p>Familiar with Reactsrap, Material UI and Bootstrap</p>
+            <p>
+              Libraries I've worked on more or less; Reactsrap, Material UI,
+              Bootstrap and Chartjs
+            </p>
           </div>
         </div>
       </div>
       <div className="flex-1-works">
         <div className="inside-1-works">
-          My Projects
           <div className="icon-1-works">
+            <h4>My Projects</h4>
             <img src="*" alt="*" />
             <p>Yes</p>
           </div>
